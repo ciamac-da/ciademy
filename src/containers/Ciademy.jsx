1b364af461977f23d54a93f0d61a7a388cdf69c4
ciamac-da <ciamac.davoudi@gmail.com>
2021-01-04 21:26:59 +0100

    Verified by Cia v1.0.9 footer component

--- src/containers/Ciademy.jsx
+++ src/containers/Ciademy.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Foorter from "../components/common/Footer.jsx";
+
 
 const Ciademy = props => {
     return (
@@ -570,135 +572,7 @@ const Ciademy = props => {
                 </div>
             </main>
 
-            <footer>
-                <div className="top-footer">
-                    <div className="container">
-                        <div className="row">
-                          
-                          
-                            <div className="col-md-3 col-sm-6 col-xs-12 footy">
-                                <section className="list">
-                                    <header>
-                                        <h4>Quick Access</h4>
-                                    </header>
-                                    <ul>
-                                        <li>
-                                            <a href="" >All Courses</a>
-                                        </li>
-                                        <li>
-                                            <a href="">Purchase Rules </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Shopping Giude </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Cooperation with Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Earning money from Ciademy </a>
-                                        </li>
-                                        <li >
-                                            <a href="" >Monthly Journal</a>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
-                            <div className="col-md-3 col-sm-6 col-xs-12 footy">
-                                <section className="list">
-                                    <header>
-                                        <h4>Quick Access</h4>
-                                    </header>
-                                    <ul>
-                                        <li>
-                                            <a href="">All Courses</a>
-                                        </li>
-                                        <li>
-                                            <a href="">Purchase Rules </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Shopping Giude </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Cooperation with Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Earning money from Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Monthly Journal</a>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
-                            <div className="col-md-3 col-sm-6 col-xs-12 footy">
-                                <section className="list">
-                                    <header>
-                                        <h4>Quick Access</h4>
-                                    </header>
-                                    <ul>
-                                        <li>
-                                            <a href="">All Courses</a>
-                                        </li>
-                                        <li>
-                                            <a href="">Purchase Rules </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Shopping Giude </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Cooperation with Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Earning money from Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Monthly Journal</a>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
-                            <div className="col-md-3 col-sm-6 col-xs-12 footy">
-                                <section className="list">
-                                    <header>
-                                        <h4>Quick Access</h4>
-                                    </header>
-                                    <ul>
-                                        <li>
-                                            <a href="">All Courses</a>
-                                        </li>
-                                        <li>
-                                            <a href="">Purchase Rules </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Shopping Giude </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Cooperation with Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Earning money from Ciademy </a>
-                                        </li>
-                                        <li>
-                                            <a href="">Monthly Journal</a>
-                                        </li>
-                                    </ul>
-                                </section>
-                            </div>
-
-
-                          
-                        </div>
-                    </div>
-                </div>
-                <div className="bottom-footer">
-                    <div className="container">
-                        <p>
-                        Copyright {" "} &copy;  {new Date().getFullYear()}
-                                    &nbsp; CIADEMY - All Rights Reserved
-                        </p>
-                    </div>
-                </div>
-            </footer>
+     <Foorter />
         </React.Fragment>
     );
 };
