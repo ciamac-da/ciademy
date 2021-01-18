a67f2f8320950fdc3075e4110286d24fea882197
ciamac-da <ciamac.davoudi@gmail.com>
2021-01-18 12:57:13 +0100

    Verified by Cia v2.3.6 nav head fixed

--- src/components/Navs/TopNav.jsx
+++ src/components/Navs/TopNav.jsx
@@ -8,24 +8,24 @@ const TopNav = () => {
                             <div className="col-sm-6 col-xs-12 myhead">
                                 <ul>
                                     <li>
-                                        <NavLink to="" exact className="myHome"     activeStyle={{color:"green", fontSize:"30px !important", textShadow: "1px 1px 1px green" }}> Home </NavLink>
-                                        <NavLink to="/aboutus" className="myHome"   activeStyle={{color:"green", fontSize:"30px !important", textShadow: "1px 1px 1px green" }}> About &nbsp; us </NavLink>
-                                        <NavLink to="/contactus" className="myHome" activeStyle={{color:"green", fontSize:"30px !important", textShadow: "1px 1px 1px green" }}> Contact &nbsp; us </NavLink>
+                                        <NavLink to="" exact     className="myHome"   activeStyle={{color:"green", fontSize:"30px !important", fontWeight:"bolder" ,  backgroundColor:"white"}}> Home </NavLink>
+                                        <NavLink to="/aboutus"   className="myHome"   activeStyle={{color:"green", fontSize:"30px !important", fontWeight:"bolder" ,  backgroundColor:"white" }}> About  us </NavLink>
+                                        <NavLink to="/contactus" className="myHome"   activeStyle={{color:"green", fontSize:"30px !important", fontWeight:"bolder" ,  backgroundColor:"white" }}> Contact  us </NavLink>
                                     </li>
                                 </ul>
                             </div>
                             <div className="col-sm-6 col-xs-12">
                                 <div className="clientarea">
                                     <div className="signin">
-                                        <img  className={{marginBottom: "10px"}} src="https://img.icons8.com/ios-filled/30/000000/user-shield.png"/>
+                                        <img  className={{marginBottom: "10px"}} src="https://img.icons8.com/ios-filled/48/000000/user-shield.png"/>
                                         &nbsp;
                                         &nbsp;
                                         &nbsp;
-                                        <NavLink to="/login" className="Login"  activeStyle={{color:"green", fontSize:"30px !important", textShadow: "1px 1px 1px green"}}> Login</NavLink> 
+                                        <NavLink to="/login" className="Login"  activeStyle={{color:"green", fontSize:"30px !important", backgroundColor:"white", textShadow: "1px 1px 1px green" , fontWeight:"lighter"}}> Login</NavLink> 
                                         &nbsp;
                                         /
                                         &nbsp;
-                                        <NavLink to="/register" className="Login" activeStyle={{color:"green", fontSize:"30px", textShadow: "1px 1px 1px green"}}> Register </NavLink>
+                                        <NavLink to="/register" className="Login" activeStyle={{color:"green", fontSize:"30px !important", backgroundColor:"white", textShadow: "1px 1px 1px green" , fontWeight:"lighter"}}> Register </NavLink>
                                     </div>
                                 </div>
                             </div>
