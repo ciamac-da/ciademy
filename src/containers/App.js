f18a03aeed32dab80350487542d4495b13bcc4c3
ciamac-da <ciamac.davoudi@gmail.com>
2021-01-26 18:46:57 +0100

    Verified by Cia v2.3.13 jquery added

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import Ciademy from './Ciademy';
 import {BrowserRouter} from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import $ from "jquery"
+
 const App = () => {
+
+useEffect(()=>{
+  require("../utils/script")
+},[])
+
+
+
     return (  
         <BrowserRouter>
         <Ciademy />
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from './containers/App';
 import 'react-toastify/dist/ReactToastify.css';
 
 render(<App />, document.getElementById("root"));
+
+
+//"homepage": "http://ciamac-da.github.io/ciademy",
