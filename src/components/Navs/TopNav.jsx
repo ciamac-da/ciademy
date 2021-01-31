16540c35fd71d7634b3cf57c2b4ecf95c99ba8a8
ciamac-da <ciamac.davoudi@gmail.com>
2021-01-31 08:51:48 +0100

    Verified by Cia v3.1.8 navbar welcome user and logout in topnav started

--- src/components/Navs/TopNav.jsx
+++ src/components/Navs/TopNav.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import { useSelector } from 'react-redux';
+import {Link, NavLink} from "react-router-dom";
+import {isEmpty} from "lodash"
+
+
 const TopNav = () => {
+
+    const user = useSelector(state => state.user)
+    
     return ( 
         <>
                   <nav>
@@ -16,6 +23,18 @@ const TopNav = () => {
                             </div>
                             <div className="col-sm-6 col-xs-12">
                                 <div className="clientarea">
+                                 {!isEmpty(user) ? (
+                                        <>
+                                        <div className="loggein">
+                                        <NavLink to="/user-profile">
+                                        Welcome {user.fullname}
+                                        </NavLink>
+                                        <NavLink to="/logout">
+                                        Logout
+                                        </NavLink>
+                                        </div>
+                                        </>
+                                 ) : (
                                     <div className="signin">
                                         <img  className={{marginBottom: "10px"}} src="https://img.icons8.com/ios-filled/48/000000/user-shield.png"/>
                                         &nbsp;
@@ -26,7 +45,8 @@ const TopNav = () => {
                                         <span className="mySlash">/</span>
                                         &nbsp;
                                         <NavLink to="/register" className="Login" activeStyle={{color:"green", fontSize:"30px !important", backgroundColor:"white", textShadow: "1px 1px 1px green" , fontWeight:"lighter"}}> Register </NavLink>
-                                    </div>
+                                        </div>
+                                 )}
                                 </div>
                             </div>
                         </div>
